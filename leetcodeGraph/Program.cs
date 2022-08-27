e7eadb571335e77117e40da11d960b6e87629038
Tofayel Ahmed <2faelahmed@gmail.com>
2022-08-27 19:01:38 +0600

    Add project files.

---
+++ leetcodeGraph/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Solution
+{
+    public int[] FindRedundantConnection(int[][] edges)
+    {
+
+    }
+}
+namespace leetcodeGraph
+{
+    public class Solution
+    {
+        public int[] FindRedundantConnection(int[][] edges)
+        {
+            return new int [edges.Length];
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
